8a6883dc5916a7fa6fc34b6eb68110fcc6c2d44f
reynvee29 <reynV1029@gmail.com>
2018-01-17 15:51:00 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyAXHefR-ZNYDOTIM5Vl1CovNmoKixALZwk",
+    authDomain: "fs1prod-b6585.firebaseapp.com",
+    databaseURL: "https://fs1prod-b6585.firebaseio.com",
+    projectId: "fs1prod-b6585",
+    storageBucket: "fs1prod-b6585.appspot.com",
+    messagingSenderId: "526029087125"
+
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-  	apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyAXHefR-ZNYDOTIM5Vl1CovNmoKixALZwk",
+    authDomain: "fs1prod-b6585.firebaseapp.com",
+    databaseURL: "https://fs1prod-b6585.firebaseio.com",
+    projectId: "fs1prod-b6585",
+    storageBucket: "fs1prod-b6585.appspot.com",
+    messagingSenderId: "526029087125"
   }
 };
